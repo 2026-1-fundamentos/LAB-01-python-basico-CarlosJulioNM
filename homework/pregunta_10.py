e2841b471896d1b92c0ca7a730d3b5051c628967
CarlosJulioNM <cnietomo@unal.edu.co>
2026-06-17 10:21:26 -0500

    pregunta_10.py

--- homework/pregunta_10.py
+++ homework/pregunta_10.py
@@ -1,11 +1,3 @@
-"""
-Escriba el codigo que ejecute la accion solicitada en cada pregunta. Los
-datos requeridos se encuentran en el archivo data.csv. En este laboratorio
-solo puede utilizar las funciones y librerias basicas de python. No puede
-utilizar pandas, numpy o scipy.
-"""
-
-
 def pregunta_10():
     """
     Retorne una lista de tuplas contengan por cada tupla, la letra de la
@@ -18,5 +10,25 @@ def pregunta_10():
      ('E', 2, 3),
      ('E', 3, 3)]
 
-
     """
+    resultado = []
+    
+    with open("files/input/data.csv", "r") as file:
+        for linea in file:
+            columnas = linea.strip().split("\t")
+            
+            # Extraemos la letra de la primera columna (índice 0)
+            letra = columnas[0]
+            
+            # Columna 4 (índice 3): separamos por comas y contamos los elementos
+            elementos_col4 = columnas[3].split(",")
+            cantidad_col4 = len(elementos_col4)
+            
+            # Columna 5 (índice 4): separamos por comas y contamos los elementos
+            elementos_col5 = columnas[4].split(",")
+            cantidad_col5 = len(elementos_col5)
+            
+            # Agregamos la tupla a nuestra lista de resultados
+            resultado.append((letra, cantidad_col4, cantidad_col5))
+            
+    return resultado
