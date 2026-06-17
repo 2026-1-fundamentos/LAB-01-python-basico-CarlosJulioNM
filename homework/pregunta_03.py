96f960e8e8451aa0a24aa5d553b852fef6d1e76a
CarlosJulioNM <cnietomo@unal.edu.co>
2026-06-17 10:18:00 -0500

    pregunta_03.py

--- homework/pregunta_03.py
+++ homework/pregunta_03.py
@@ -1,11 +1,3 @@
-"""
-Escriba el codigo que ejecute la accion solicitada en cada pregunta. Los
-datos requeridos se encuentran en el archivo data.csv. En este laboratorio
-solo puede utilizar las funciones y librerias basicas de python. No puede
-utilizar pandas, numpy o scipy.
-"""
-
-
 def pregunta_03():
     """
     Retorne la suma de la columna 2 por cada letra de la primera columna como
@@ -15,3 +7,19 @@ def pregunta_03():
     [('A', 53), ('B', 36), ('C', 27), ('D', 31), ('E', 67)]
 
     """
+    sumas = {}
+    
+    with open("files/input/data.csv", "r") as file:
+        for linea in file:
+            columnas = linea.strip().split("\t")
+            letra = columnas[0]
+            valor = int(columnas[1]) 
+            
+            if letra in sumas:
+                sumas[letra] += valor
+            else:
+                sumas[letra] = valor
+                
+    resultado_ordenado = sorted(sumas.items())
+    
+    return resultado_ordenado
