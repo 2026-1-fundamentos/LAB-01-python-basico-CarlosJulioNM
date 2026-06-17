5750b47c04cbb9f4f4197d5850097b8cbb6704b7
CarlosJulioNM <cnietomo@unal.edu.co>
2026-06-17 10:16:59 -0500

    pregunta_01.py

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,11 +1,3 @@
-"""
-Escriba el codigo que ejecute la accion solicitada en cada pregunta. Los
-datos requeridos se encuentran en el archivo data.csv. En este laboratorio
-solo puede utilizar las funciones y librerias basicas de python. No puede
-utilizar pandas, numpy o scipy.
-"""
-
-
 def pregunta_01():
     """
     Retorne la suma de la segunda columna.
@@ -14,3 +6,12 @@ def pregunta_01():
     214
 
     """
+    suma_total = 0
+    
+    # La ruta correcta basada en tu explorador de archivos
+    with open("files/input/data.csv", "r") as file:
+        for linea in file:
+            columnas = linea.strip().split("\t")
+            suma_total += int(columnas[1])
+            
+    return suma_total
