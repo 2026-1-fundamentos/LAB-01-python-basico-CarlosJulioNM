707bc83371393c449e57dc04ba33a27126a91207
CarlosJulioNM <cnietomo@unal.edu.co>
2026-06-17 10:17:33 -0500

    pregunta_02.py

--- homework/pregunta_02.py
+++ homework/pregunta_02.py
@@ -1,11 +1,3 @@
-"""
-Escriba el codigo que ejecute la accion solicitada en cada pregunta. Los
-datos requeridos se encuentran en el archivo data.csv. En este laboratorio
-solo puede utilizar las funciones y librerias basicas de python. No puede
-utilizar pandas, numpy o scipy.
-"""
-
-
 def pregunta_02():
     """
     Retorne la cantidad de registros por cada letra de la primera columna como
@@ -15,3 +7,20 @@ def pregunta_02():
     [('A', 8), ('B', 7), ('C', 5), ('D', 6), ('E', 14)]
 
     """
+    conteo = {}
+    
+    # Ruta corregida a la carpeta input
+    with open("files/input/data.csv", "r") as file:
+        for linea in file:
+            columnas = linea.strip().split("\t")
+            letra = columnas[0]
+            
+            if letra in conteo:
+                conteo[letra] += 1
+            else:
+                conteo[letra] = 1
+                
+    resultado_ordenado = sorted(conteo.items())
+    
+    return resultado_ordenado
+    
